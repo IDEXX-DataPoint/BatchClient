93ded0a476f29d26700dd3b2f661549b45d8feb5
= <esoeralaya@gmail.com>
2018-10-07 16:01:15 -0500

    Add embedded resource utility

---
+++ DataPointBatchClient/Utility/EmbeddedResource.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace DataPointBatchClient.Utility
+{
+    public static class EmbeddedResource
+    {
+        public static string Get(string resourceName)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+
+            using (var stream = assembly.GetManifestResourceStream(resourceName))
+            using (var reader = new StreamReader(stream ?? throw new InvalidOperationException()))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+    }
+}

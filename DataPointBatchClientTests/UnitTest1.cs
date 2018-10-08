269f054a100d3ba780705d50c75f8d351a204eec
Ernest <esoeralaya@gmail.com>
2018-10-07 20:44:07 -0500

    Update tests

--- DataPointBatchClientTests/UnitTest1.cs
+++ DataPointBatchClientTests/UnitTest1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using DataPointBatchClient.Utility;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace DataPointBatchClientTests
@@ -7,8 +7,17 @@ namespace DataPointBatchClientTests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void BatchApiUtility_RetrieveToken()
         {
+            // todo link or copy app.config
+            Assert.AreNotEqual(BatchApiUtility.Token, null);
+        }
+
+        [TestMethod]
+        public void EmbeddedResource_RetrieveResource()
+        {
+            var sql = EmbeddedResource.Get("DataPointBatchClient.Scripts.InsertPatient.sql");
+            Assert.AreNotEqual(sql, null);
         }
     }
 }

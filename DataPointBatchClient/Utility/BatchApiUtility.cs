0ee353c2ab842eafeb56dd5c8f6c801fd1958462
Ernest <esoeralaya@gmail.com>
2018-10-07 22:53:23 -0500

    Move filter to utility

--- DataPointBatchClient/Utility/BatchApiUtility.cs
+++ DataPointBatchClient/Utility/BatchApiUtility.cs
@@ -3,18 +3,34 @@ using RestSharp;
 
 namespace DataPointBatchClient.Utility
 {
+    /// <summary>
+    /// Variables which only be initialized once for all resources
+    /// </summary>
     public static class BatchApiUtility
     {
         public static readonly RestClient Client;
+        public static readonly string Filter;
         public static readonly string Token;
-        public static int Top => 10; // for debugging
+        public static int Top => 5000; // for debugging
 
         private const string Endpoint = "https://io.datapointapi.com";
 
         static BatchApiUtility()
         {
+            // todo test singleton constructor called only once
             Client = new RestClient(Endpoint);
+            Filter = GetFilter();
             Token = GetToken();
+        }
+
+        private static string GetFilter()
+        {
+            var siteId = Properties.Settings.Default.SiteId;
+            var lastUpdated = Properties.Settings.Default.LastUpdated;
+
+            return string.IsNullOrEmpty(lastUpdated)
+                ? $"siteId eq {siteId}"
+                : $"siteId eq {siteId} and dpModifiedDate gt {lastUpdated}";
         }
 
         private static string GetToken()
@@ -22,7 +38,6 @@ namespace DataPointBatchClient.Utility
             var request = new RestRequest("token", Method.POST);
             request.AddHeader("Content-Type", "application/x-www-form-urlencoded");
             request.AddParameter("grant_type", "password");
-            
             request.AddParameter("username", Properties.Settings.Default.Username);
             request.AddParameter("password", Properties.Settings.Default.Password);
 

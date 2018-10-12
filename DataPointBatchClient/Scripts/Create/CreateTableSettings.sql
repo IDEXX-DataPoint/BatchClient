97284843efbfd70f72ef1c04a443a52bbb0f0533
Kenneth Crawford <kmc059000@gmail.com>
2018-10-11 19:42:31 -0500

    settings table

--- DataPointBatchClient/Scripts/Create/CreateTableSettings.sql
+++ DataPointBatchClient/Scripts/Create/CreateTableSettings.sql
@@ -5,12 +5,11 @@ SET QUOTED_IDENTIFIER ON
 GO
 
 CREATE TABLE [dbo].[Settings](
-	[SettingsId] [uniqueidentifier] NOT NULL,
-	[ResourceName] [varchar](50) NULL,
+	[ResourceName] [varchar](50),
 	[LastUpdated] [datetime] NULL,
  CONSTRAINT [PK_Settings] PRIMARY KEY CLUSTERED 
 (
-	[SettingsId] ASC
+	[ResourceName] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
